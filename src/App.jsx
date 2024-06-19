9d538c1eca97751ec98749ee4b1619d970742558
Joyce Wambui <joycewambui@gmail.com>
2024-06-19 08:19:16 +0300

    app

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css';
 import ProfileSeg from './components/Profile-1';
-import mary from './assets/mary.jpg';
+import mary from './assets/Mary2S.jpg';
 import james from './assets/James.png'; 
 import lilian from './assets/lilian.jpg';
 import martin from './assets/martin.jpg';
@@ -9,31 +9,33 @@ function App() {
   return (
     <div className='ProfileSection'>
       <ProfileSeg
+      
         profilePic={mary}
         name="Mary Scott"
         age={28}
-        bio="I am a passionate software developer"
+        bio="Mary Scott is a seasoned software engineer with over 10 years of experience in building robust web applications. With a Bachelor’s degree in Computer Science from MIT and a Master’s degree in Data Science from Stanford, Mary has a strong foundation in both theoretical and practical aspects of technology. At Google, she led a team that developed an award-winning machine learning algorithm that improved search engine accuracy by 30%."
+        
       />
 
       <ProfileSeg
         profilePic={james}
         name="James Scott"
         age={25}
-        bio="I am a passionate software engineer"
+        bio="James Scott is a visionary product manager with a track record of driving growth for SaaS products. At Salesforce, he spearheaded the development of a CRM tool that increased customer retention by 25%. James is also an active member of the Product Management community, contributing to blogs and hosting webinars."
       />
 
       <ProfileSeg
         profilePic={lilian}
         name="Lilian Ivy"
         age={31}
-        bio="I am a passionate software engineer"
+        bio="Lilian Ivy is an innovative tech leader and entrepreneur with a keen eye for emerging technologies. With a degree in Software Engineering from Carnegie Mellon University, Lilian co-founded a successful startup that developed a cutting-edge IoT platform to streamline smart home integration. Her work has been featured in TechCrunch and Wired. As a speaker at international tech conferences, she shares insights on blockchain’s potential to revolutionize cybersecurity."
       />
 
       <ProfileSeg
         profilePic={martin}
         name="Martin Musk"
         age={24}
-        bio="I am a passionate software engineer"
+        bio="Martin Musk is a cybersecurity expert with a decade of experience in protecting enterprise-level networks. As a Certified Ethical Hacker, he has helped Fortune 500 companies thwart cyber threats. Martin’s innovative approach to security has earned him recognition from the Cybersecurity Excellence Awards"
       />
     </div>
   );
--- src/components/Profile-1.jsx
+++ src/components/Profile-1.jsx
@@ -2,8 +2,11 @@ import React from 'react';
 
 const ProfileSeg = ({ age, name, profilePic, bio }) => {
     return (
-        <div className="pic">
+        <div className="section">
+            <div className='pic'>
             <img src={profilePic} alt={name} />
+            </div>
+            
             <div className="content">
                 <h3>{name}</h3>
                 <p>Age: {age}</p>
